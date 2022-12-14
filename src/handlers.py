4377d54321f133d674e688046d4d230b3dc8f1f2
alejack9 <25297222+alejack9@users.noreply.github.com>
2022-12-14 22:43:50 +0100

    add swap information

--- src/handlers.py
+++ src/handlers.py
@@ -31,10 +31,12 @@ def send_stats(update: Update, context: CallbackContext):
   if get(update.effective_user.id):
     return context.bot.send_message(chat_id=update.effective_chat.id, text="Not allowed\. Use `/ask` to ask permission\.", parse_mode='MarkdownV2')
 
-  cpu = psutil.cpu_percent()
+  cpu = psutil.cpu_percent(interval=1)
   stats = f"{get_emoji(cpu)} CPU percentage: {cpu} %\n"
-  mem = psutil.virtual_memory()
-  stats += f"{get_emoji(mem.used / mem.total * 100)} Memory: {round(mem.used / mem.total * 100, 2)} %\n"
+  mem = psutil.virtual_memory().percent
+  stats += f"{get_emoji(mem)} Memory: {round(mem, 2)} %\n"
+  swap = psutil.swap_memory().percent
+  stats += f"{get_emoji(swap)} Swap: {round(swap, 2)} %\n"
   disk = psutil.disk_usage('/')
   stats += f"{get_emoji(disk.used / disk.total * 100)} Disk quota (/): {round(disk.used / disk.total * 100, 2)} % "
   
